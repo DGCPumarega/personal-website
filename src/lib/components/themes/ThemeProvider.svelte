a38d2de2defe8bba3b8102e9d9010b464bc83dc7
DGCPumarega <gabrielpumarega@yahoo.com>
2026-01-24 15:14:34 -0700

    Add ThemeProvider component to handle multiple themes

---
+++ src/lib/components/themes/ThemeProvider.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import ThemeSpace from "$lib/components/themes/ThemeSpace.svelte";
+
+	let { children } = $props();
+</script>
+
+<div>
+  <!-- 
+    TODO: More themes 
+   -->
+  <ThemeSpace>{@render children()}</ThemeSpace>
+</div>
