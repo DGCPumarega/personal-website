6b51e4bd2d6d3d968a41123933fe77046a656199
DGCPumarega <gabrielpumarega@yahoo.com>
2026-01-26 13:33:25 -0700

    Add CatBoxIdle animated sprite

---
+++ src/lib/animated-sprites/CatBoxIdle.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { T, Canvas} from "@threlte/core";
+  import { AnimatedSpriteMaterial } from "@threlte/extras";
+</script>
+
+<div class="h-8 w-8">
+  <Canvas>
+    <T.Sprite
+      scale={[7,7,1]}
+    >
+      <AnimatedSpriteMaterial
+        textureUrl="/src/lib/assets/cat-box-idle.png"
+        totalFrames={4}
+        fps={4}
+      />
+    </T.Sprite>
+  </Canvas>
+</div>
