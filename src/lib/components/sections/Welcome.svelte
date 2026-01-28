78dd20bae41409f29314a4dc0a5786d12d16a625
DGCPumarega <gabrielpumarega@yahoo.com>
2026-01-28 15:47:36 -0700

    WIP: Replacing threlte with CSS for animated sprites

--- src/lib/components/sections/Welcome.svelte
+++ src/lib/components/sections/Welcome.svelte
@@ -6,7 +6,10 @@
 <section class="{className} font-oxanium">
   <div class="flex gap-x-2 align-middle justify-center">
     <h2 class="text-3xl text-center uppercase font-bold">Welcome</h2>
-    <CatBoxIdle />
+    <!--
+      TODO: Replace Threlte with CSS for animated sprites 
+      <CatBoxIdle />
+    -->
   </div>
   <div class="flex flex-col gap-y-4 leading-5">
     <p>
