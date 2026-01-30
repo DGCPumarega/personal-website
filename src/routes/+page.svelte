66c310b08d168311af0d4dde1089e46fbb5593c6
DGCPumarega <gabrielpumarega@yahoo.com>
2026-01-29 22:13:13 -0700

    Move main tag to layout, update styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import Welcome from "$lib/components/sections/Welcome.svelte"
-  import Outlinks from "$lib/components/sections/Outlinks.svelte";
-  import Projects from "$lib/components/sections/Projects.svelte";
-  import Chip8 from "$lib/components/sections/Chip8.svelte";
+  import Welcome from "$lib/components/home-sections/Welcome.svelte"
+  import Outlinks from "$lib/components/home-sections/Outlinks.svelte";
+  import Projects from "$lib/components/home-sections/Projects.svelte";
+  import Chip8 from "$lib/components/home-sections/Chip8.svelte";
 </script>
 
-<main class="grid grid-cols-1 gap-2 mx-4 mt-3 sm:grid-cols-3 lg:mx-auto lg:grid-cols-4 lg:w-250">
+<div class="grid grid-cols-1 gap-2 mx-4 sm:grid-cols-3 lg:mx-auto lg:grid-cols-4 lg:w-250">
   <Welcome class="sm:col-span-2 lg:col-span-3" />
   <Projects class="" />
   <Outlinks class="" />
@@ -14,4 +14,4 @@
   <section class="flex justify-center">Placeholder 3</section>
   <section class="flex justify-center">Placeholder 4</section>
   <section class="flex justify-center">Placeholder 5</section>
-</main>
+</div>
