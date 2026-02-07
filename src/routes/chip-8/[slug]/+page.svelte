cdd0f64764386d712e9bfdbfc1e88f9157ee47d8
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-06 18:41:44 -0700

    Remove the temporary code used to debug the Display component

--- src/routes/chip-8/[slug]/+page.svelte
+++ src/routes/chip-8/[slug]/+page.svelte
@@ -7,27 +7,21 @@
 
 	let vm: VirtualMachine = $state(createVM());
 
-	let x = $state(0);
-	let y = $state(0);
 	onMount(() => {
 		loadFontSet(vm, FONT_SET);
 		loadRom(vm, data.bytes);
 
 		// TODO: set up more accurate clock
-		setInterval(() => {
-			// runLoop(vm);
-
-			vm.display[x][y] = true;
-			x += 1;
-			if(x >= vm.display.length) { x = 0; y += 1}
-		}, 800);
+		setInterval(() => { 
+			runLoop(vm); 
+		}, 1000);
 	});
 
 
 	let { data }: PageProps = $props();
 </script>
 
-<section class="lg:w-250">
-  <h2>{data.title}</h2>
+<section class="lg:w-250 mx-auto">
+	<h2>{data.title}</h2>
 	<Display pixels={vm.display} />
 </section>
