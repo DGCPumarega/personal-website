80827205d5eea07b503313fa93cb2f625e23c6de
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-09 00:12:14 -0700

    Add KeyboardEvent listeners

--- src/routes/chip-8/[slug]/+page.svelte
+++ src/routes/chip-8/[slug]/+page.svelte
@@ -17,6 +17,28 @@
 		}, 1000);
 	});
 
+	const handleKeyDown = (event: KeyboardEvent) => {
+		switch(event.code) {
+			case "Digit1": vm.keypad = 0x1; break;
+			case "Digit2": vm.keypad = 0x2; break;
+			case "Digit3": vm.keypad = 0x3; break;
+			case "Digit4": vm.keypad = 0xC; break;
+			case "KeyQ": vm.keypad = 0x4; break;
+			case "KeyW": vm.keypad = 0x5; break;
+			case "KeyE": vm.keypad = 0x6; break;
+			case "KeyR": vm.keypad = 0xD; break;
+			case "KeyA": vm.keypad = 0x7; break;
+			case "KeyS": vm.keypad = 0x8; break;
+			case "KeyD": vm.keypad = 0x9; break;
+			case "KeyF": vm.keypad = 0xE; break;
+			case "KeyZ": vm.keypad = 0xA; break;
+			case "KeyX": vm.keypad = 0x0; break;
+			case "KeyC": vm.keypad = 0xB; break;
+			case "KeyV": vm.keypad = 0xF; break;
+			default: vm.keypad = null; break;
+		}
+	}
+	const handleKeyUp = () => { vm.keypad = null; }
 
 	let { data }: PageProps = $props();
 </script>
@@ -25,3 +47,8 @@
 	<h2>{data.title}</h2>
 	<Display pixels={vm.display} />
 </section>
+
+<svelte:window
+	on:keydown|preventDefault={handleKeyDown}
+	on:keyup|preventDefault={handleKeyUp}
+/>
