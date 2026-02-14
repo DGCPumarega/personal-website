5cd8f58297533fba4515fe21b1528cb3f7e0d5e4
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-14 13:26:00 -0700

    WIP: starting guestbook section

---
+++ src/lib/components/home-sections/Guestbook.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import * as Form from "$lib/components/ui/form";
+
+  let {class: className}: {class?: string} = $props();
+</script>
+
+<section class="{className} font-oxanium flex flex-col gap-y-2">
+  <h2 class="text-xl sm:text-3xl text-center uppercase font-bold">Leave a Message</h2>
+  <div id="message-display" class="min-h-1/2"></div>
+  <form>
+
+  </form>
+</section>
