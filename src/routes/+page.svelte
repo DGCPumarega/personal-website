04c10e2a30fe45ad2f9d9535fb662ae4961f143f
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-14 13:24:32 -0700

    WIP: Editing homepage sections and positioning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,17 @@
   import Outlinks from "$lib/components/home-sections/Outlinks.svelte";
   import Projects from "$lib/components/home-sections/Projects.svelte";
   import Chip8 from "$lib/components/home-sections/Chip8.svelte";
+  import Blog from "$lib/components/home-sections/Blog.svelte";
+  import Guestbook from "$lib/components/home-sections/Guestbook.svelte";
 </script>
 
-<div class="grid grid-cols-1 gap-2 mx-4 sm:grid-cols-3 lg:mx-auto lg:grid-cols-4 lg:w-250">
-  <Welcome class="sm:col-span-2 lg:col-span-3" />
-  <Projects class="" />
-  <Outlinks class="" />
-  <Chip8 />
-  <section class="flex justify-center">Placeholder 2</section>
-  <section class="flex justify-center">Placeholder 3</section>
-  <section class="flex justify-center">Placeholder 4</section>
-  <section class="flex justify-center">Placeholder 5</section>
+<div class="grid grid-cols-1 gap-2 mx-4 sm:grid-cols-3 lg:mx-auto lg:grid-cols-12 lg:grid-rows-6 lg:w-250">
+  <Welcome class="sm:col-span-2 lg:col-span-9 lg:row-span-2" />
+  <Blog class="lg:row-span-3 lg:col-span-3" />
+  <Projects class="lg:row-span-2 lg:col-span-6" />
+  <section class="flex justify-center lg:col-span-3">Placeholder 2</section>
+  <Chip8 class="lg:col-span-3"/>
+  <Outlinks class="lg:col-span-3" />
+  <Guestbook class="lg:col-span-6 lg:row-span-2"/>
+  <section class="flex justify-center lg:col-span-6 lg:row-span-2">Placeholder 3</section>
 </div>
