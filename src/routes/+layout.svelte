bd5eed4f6009f40b782e6e5fec27723c106082c4
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-14 13:22:29 -0700

    Add footer and edit header styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
 </svelte:head>
 
 <ThemeProvider>
-	<header class="w-full h-20 flex flex-col justify-end text-white mx-4 mb-3 lg:mx-auto lg:w-250">
+	<header class="w-full h-15 sm:h-20 flex flex-col justify-end text-white mx-4 mb-3 lg:mx-auto lg:w-250">
 		<div class="relative w-min pl-1">
 			<a href="/">
 				<h1 class="uppercase text-xl sm:text-4xl font-press-start neon-text z-1" data-text="DGCPumarega.dev">DGCPumarega.dev</h1>
@@ -28,6 +28,7 @@
 	<main>
 		{@render children()}
 	</main>
+	<footer class="w-full h-20 mx-4 lg:mx-auto lg:w-250 mt-3"></footer>
 </ThemeProvider>
 
 <style>
