d1db7c1b1ff7432304fe25eef7f79c195f3dc6c2
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-17 20:39:11 -0700

    Add 3D computer model

---
+++ src/lib/components/home-sections/Computer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Canvas } from "@threlte/core";
+  import IBM5155 from "$lib/components/scenes/IBM5155.svelte";
+</script>
+
+<Canvas>
+  <IBM5155 />
+</Canvas>
---
+++ src/lib/components/scenes/IBM5155.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { T, useTask } from "@threlte/core";
+  import { GLTF, interactivity } from "@threlte/extras";
+
+  let rotation = $state(0);
+
+  useTask((delta) => {
+    rotation += delta;
+  });
+
+  interactivity();
+</script>
+
+<T.PerspectiveCamera 
+  position={[10,10,10]}
+  oncreate={(ref) => {
+    ref.lookAt(0,0,0)
+  }}
+/>
+<T.AmbientLight intensity={0.5} />
+<GLTF
+  position={[0, 0, 0]}
+  rotation.x={rotation} rotation.y={rotation} rotation.z={rotation}
+  scale={2.35}
+  url="/models/IBM5155.glb"
+/>
+
+<!--
+<Text 
+  text={"i luv compootr"}
+  scale={7}
+  position={[-2, -2.5, 0]}
+  textAlign={"center"}
+/>
+-->
