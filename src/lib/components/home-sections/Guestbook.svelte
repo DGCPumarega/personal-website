6f36d1c07404b5d4a9b4cd41903bf67604b61ac6
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-17 22:48:47 -0700

    Add id for href purposes

--- src/lib/components/home-sections/Guestbook.svelte
+++ src/lib/components/home-sections/Guestbook.svelte
@@ -4,7 +4,7 @@
   let {class: className}: {class?: string} = $props();
 </script>
 
-<section class="{className} font-oxanium flex flex-col gap-y-2">
+<section id="guestbook" class="{className} font-oxanium flex flex-col gap-y-2">
   <h2 class="text-xl sm:text-3xl text-center uppercase font-bold">Leave a Message</h2>
   <div id="message-display" class="min-h-1/2"></div>
   <form>
