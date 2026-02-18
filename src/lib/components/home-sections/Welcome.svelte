78112b42836af440b7804a62e0b06ec1058f5463
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-17 22:47:55 -0700

    Edit welcome content

--- src/lib/components/home-sections/Welcome.svelte
+++ src/lib/components/home-sections/Welcome.svelte
@@ -1,27 +1,30 @@
 <script lang="ts">
-  import CatBoxIdle from "$lib/animated-sprites/CatBoxIdle.svelte";
   let {class: className}: {class?: string} = $props();
 </script>
 
 <section class="{className} font-oxanium">
   <div class="flex gap-x-2 align-middle justify-center mb-4">
     <h2 class="text-xl sm:text-3xl text-center uppercase font-bold">Welcome</h2>
-    <CatBoxIdle class="hidden sm:block" />
   </div>
   <div class="flex flex-col gap-y-4 leading-5">
+    <p>...to one of <span class="font-bold">the</span> websites of all time.</p>
     <p>
-      ...to my corner of the internet!
+      I'm a (somewhat new) programmer currently living in <span class="font-bold text-red-300">Calgary, AB</span>. 
+      I studied <span class="font-bold text-red-300">Software Development</span> at the <span class="font-bold text-red-300">Northern Alberta Institute of Technology (NAIT)</span> in Edmonton, and now I'm using what I've learned to make stuff on the internet! 
     </p>
     <p>
-      I'm a programmer (and chronic procrastinator) currently living in <span class="font-bold text-red-300">Calgary, AB</span>. 
-      I studied <span class="font-bold text-red-300">Software Development</span> at the <span class="font-bold text-red-300">Northern Alberta Institute of Technology (NAIT)</span> in Edmonton, and now I'm using what I've learned to make stuff on the internet. 
+      I was inspired to make my own website after coming across several 2000s-style pages hosted on places like 
+      <a class="text-blue-200 underline underline-offset-2" href="https://neocities.org/" target="_blank">Neocities</a> 
+      and 
+      <a class="text-blue-200 underline underline-offset-2" href="https://nekoweb.org/" target="_blank">Nekoweb</a>. 
+      It was great to see so many sites designed in such creative and personal ways! 
+      They reminded me of how awesome the old internet used to be - back when people used to surf the web instead of visit the same old apps and social media pages.
     </p>
     <p>
-      I was inspired to create my own personal site after coming across some awesome, 2000s-style pages on the indie web. 
-      I'm using more modern dev tools and design practices for this project, but hopefully, I'll be able to capture a bit of that indie web vibe!
-    </p>
-    <p>
-      Enjoy your stay!
+      This place is <span class="text-yellow-300">still under construction</span>, but I hope you have fun exploring this website!
+      And don't forget to leave a comment or some constructive criticism in the 
+      <a class="text-blue-200 underline underline-offset-2" href="#guestbook">guestbook</a> 
+      before you go!
     </p>
   </div>
 </section>
