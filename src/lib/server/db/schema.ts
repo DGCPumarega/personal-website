ca0f991127220bd78c79743b66cee67623341396
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-20 23:53:49 -0700

    Add db name for createdAt column and add relations

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,10 @@
 import { pgTable, serial, integer, text, timestamp } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 
 export const messages = pgTable('messages', {
 	id: serial('id').primaryKey(),
 	username: text().notNull(),
-	createdAt: timestamp().notNull().defaultNow(),
+	createdAt: timestamp('created_at').notNull().defaultNow(),
 	email: text(),
 	content: text().notNull(),
 });
@@ -12,6 +13,17 @@ export const replies = pgTable('replies', {
 	id: serial('id').primaryKey(),
 	messageId: integer().references(() => messages.id).notNull(),
 	username: text().notNull(),
-	createdAt: timestamp().notNull().defaultNow(),
+	createdAt: timestamp('created_at').notNull().defaultNow(),
 	content: text().notNull(),
 });
+
+export const messagesRelations = relations(messages, ({ many }) => ({
+	replies: many(replies),
+}));
+
+export const repliesRelations = relations(replies, ({ one }) => ({
+	messages: one(messages, {
+		fields: [replies.messageId],
+		references: [messages.id]
+	}),
+}))
