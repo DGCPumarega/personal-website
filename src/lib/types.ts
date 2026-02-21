1a4618041154e5378667d30a39ab0d1fc0dd4afe
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-20 23:43:24 -0700

    Add GuestbookMessage type and change name of Post type to BlogPost

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,13 @@
-export type Post = {
+export type BlogPost = {
   title: string;
   description: string;
   date: string;
   isPublished: boolean;
   slug: string;
+}
+
+export type GuestbookMessage = {
+  username: string,
+  content: string,
+  replies?: { username: string, content: string }[],
 }
