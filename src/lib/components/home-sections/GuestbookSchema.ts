1b7536b527eb7544f58ef5bd4a832e78ec1dc17c
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-20 23:47:41 -0700

    Edit error messages for Guestbook Form

--- src/lib/components/home-sections/GuestbookSchema.ts
+++ src/lib/components/home-sections/GuestbookSchema.ts
@@ -1,9 +1,13 @@
 import { z } from "zod";
  
 export const formSchema = z.object({
-  username: z.string().trim().min(1).max(50),
+  username: z.string().trim()
+    .min(1, { error: "Please Enter a Username" })
+    .max(50, { error: "Please Limit Username to 50 Characters"}),
   email: z.email().optional(),
-  message: z.string().trim().min(1).max(255),
+  message: z.string().trim()
+    .min(1, { error: "Please Enter a Message"})
+    .max(255, { error: "Please Limit Message to 255 Characters"}),
 });
  
 export type FormSchema = typeof formSchema;
