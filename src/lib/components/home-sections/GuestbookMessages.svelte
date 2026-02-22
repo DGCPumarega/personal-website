c6c9db442e7e236235b63f859c8dd59bb4799e48
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-21 23:41:49 -0700

    Edit styles

--- src/lib/components/home-sections/GuestbookMessages.svelte
+++ src/lib/components/home-sections/GuestbookMessages.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
   import type { GuestbookMessage } from "$lib/types";
+  import { formatDate } from "$lib/utils";
 
   let { class: className, messages }: { class?: string, messages: GuestbookMessage[] } = $props();
 </script>
 
 <div class={className}>
-  <ul class="flex flex-col gap-y-4 p-2 h-full overflow-y-scroll">
+  <ul class="flex flex-col gap-y-4 h-full overflow-y-scroll">
     {#each messages as message}
-      <li class="w-full flex justify-between">
-        <p class="w-1/2">{message.username}</p>
-        <p class="w-1/2">{message.content}</p>
+      <li class="p-2 pb-3">
+        <div class="mb-1 pb-1 leading-4 border-b border-b-yellow-200">
+          <p class="font-semibold">{message.username}</p>
+          <p class="text-xs uppercase italic">{formatDate(message.createdAt, "short")}</p>
+        </div>
+        <p class="leading-5 text-sm">{message.content}</p>
       </li>
     {/each}
   </ul>
