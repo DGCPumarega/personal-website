2a1779bd72b19519bf53d35d8b7c7759465869bc
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-23 13:28:18 -0700

    Minor edits to styles

--- src/lib/components/home-sections/GuestbookMessages.svelte
+++ src/lib/components/home-sections/GuestbookMessages.svelte
@@ -5,10 +5,10 @@
   let { class: className, messages }: { class?: string, messages: GuestbookMessage[] } = $props();
 </script>
 
-<div class={className}>
-  <ul class="flex flex-col gap-y-4 h-full overflow-y-scroll">
+<div class="{className} font-oxanium">
+  <ul class="flex flex-col gap-y-2 h-full overflow-y-scroll">
     {#each messages as message}
-      <li class="p-2 pb-3">
+      <li class="p-2">
         <div class="mb-1 pb-1 leading-4 border-b border-b-yellow-200">
           <p class="font-semibold">{message.username}</p>
           <p class="text-xs uppercase italic">{formatDate(message.createdAt, "short")}</p>
