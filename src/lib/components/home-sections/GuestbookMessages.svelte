47930ba05ec2cd29c6fa8cb0b4f256338d7af22e
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-26 14:51:44 -0700

    Minor edit to styles

--- src/lib/components/home-sections/GuestbookMessages.svelte
+++ src/lib/components/home-sections/GuestbookMessages.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="{className} font-oxanium">
-  <ul class="flex flex-col gap-y-2 h-full overflow-y-auto scrollable">
+  <ul class="flex flex-col gap-y-2 h-full overflow-y-auto scrollable pb-5">
     {#each messages as message}
       <li class="p-2">
         <div class="mb-1 pb-1 leading-4 border-b border-b-yellow-200">
