2d074ee161be0e362d5aa8366db99a766dd04175
DGCPumarega <gabrielpumarega@yahoo.com>
2026-02-26 14:50:42 -0700

    Add util functions for Spotify API auth process

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,3 +19,22 @@ export function formatDate(dateParam: string | Date, style: "short" | "long" = "
 		{ year: "numeric", month: style, day: "numeric" }
 	).format(date);
 }
+
+export function generateRandomString(length: number) {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
+export async function sha256(plain: string) {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(plain)
+  return await window.crypto.subtle.digest('SHA-256', data)
+}
+
+export function base64encode(input: ArrayBuffer) {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
