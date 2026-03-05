aeb4d81daecf2cbd592a01520f47aa04525b1692
DGCPumarega <gabrielpumarega@yahoo.com>
2026-03-05 15:32:46 -0700

    Nit: Fix formatting

--- src/lib/components/home-sections/GuestbookSchema.ts
+++ src/lib/components/home-sections/GuestbookSchema.ts
@@ -8,7 +8,8 @@ export const formSchema = z.object({
     protocol:/^https?$/,
     hostname: z.regexes.domain,
     error: "URL must start with http/https and use a valid domain name",
-  },).optional(),
+    })
+    .optional(),
   message: z.string().trim()
     .min(1, { error: "Please Enter a Message"})
     .max(255, { error: "Please Limit Message to 255 Characters"}),
