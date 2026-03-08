629ca5c87edc2119d134b67ea93b86d6845e25ac
DGCPumarega <gabrielpumarega@yahoo.com>
2026-03-07 19:12:04 -0700

    Fix grid styles and add status component to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,34 +9,34 @@
   import Guestbook from "$lib/components/home-sections/Guestbook.svelte";
   import Computer from "$lib/components/home-sections/Computer.svelte";
   import Spotify from "$lib/components/home-sections/Spotify.svelte";
-  import Gallery from "$lib/components/home-sections/Gallery.svelte";
+  import Status from "$lib/components/home-sections/Status.svelte";
 
   let { data }: PageProps = $props();
 </script>
 
-<div class="grid grid-cols-1 gap-2 mx-4 opacity-60 sm:grid-cols-3 lg:mx-auto lg:grid-cols-12 lg:w-250">
-  <Welcome class="sm:col-span-2 lg:col-span-9 lg:row-span-2" />
+<div class="grid grid-cols-12 gap-2 mx-4 opacity-60 lg:mx-auto lg:w-250">
+  <Welcome class="col-span-12 lg:col-span-9 lg:row-span-2" />
   <Blog
-    class="lg:row-span-3 lg:col-span-3" 
+    class="col-span-12 sm:col-span-4 lg:row-span-3 lg:col-span-3" 
     posts={data.blogPosts}
   />
-  <Projects class="lg:row-span-2 lg:col-span-6" />
-  <section class="flex flex-col align-middle justify-center lg:col-span-3 border-none! bg-transparent! p-0!">
+  <Projects class="col-span-12 sm:col-span-8 lg:row-span-2 lg:col-span-6" />
+  <Chip8 class="col-span-12 sm:col-span-4 lg:col-span-3"/>
+  <section class="flex flex-col align-middle justify-center col-span-12 sm:col-span-4 lg:col-span-3 border-none! bg-transparent! p-0!">
     <h2 class="sr-only">Computer</h2>
     <Computer class="w-full h-full" />
   </section>
-  <Chip8 class="lg:col-span-3"/>
-  <Outlinks class="lg:col-span-3" />
+  <Outlinks class="col-span-12 sm:col-span-4 lg:col-span-3" />
   <Guestbook
-    class="lg:col-span-6 lg:row-span-2"
+    class="col-span-12 lg:col-span-6 lg:row-span-2"
     formProp={data.form}
     messages={data.guestbookMessages} 
   />
+  <Status class="col-span-12 lg:col-span-6" status={data.status}/>
   <Spotify
-    class="lg:col-span-6"
+    class="col-span-12 lg:col-span-6"
     nowPlaying={data.nowPlaying}
     recentTracks={data.recentTracks}
     topTracks={data.topTracks}
   />
-  <Gallery class="flex lg:col-span-6 h-68" />
 </div>
