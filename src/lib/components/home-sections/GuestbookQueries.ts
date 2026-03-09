fd78f682b9fd4a53d549ef504836ebcd3a7dd50b
DGCPumarega <gabrielpumarega@yahoo.com>
2026-03-09 15:59:03 -0600

    Fetch replies from DB and append them to their corresponding messages

--- src/lib/components/home-sections/GuestbookQueries.ts
+++ src/lib/components/home-sections/GuestbookQueries.ts
@@ -1,22 +1,34 @@
-import type { GuestbookMessage } from "$lib/types";
+import type { GuestbookMessage, GuestbookReply } from "$lib/types";
 import { db } from "$lib/server/db";
-import { messages } from "$lib/server/db/schema";
-import { desc } from "drizzle-orm";
+import { messages, replies } from "$lib/server/db/schema";
+import { eq, desc } from "drizzle-orm";
 
 export const getGuestbookMessages = async () => {
-  let savedMessages = await db
-  .select({
-    username: messages.username,
-    createdAt: messages.createdAt,
-    content: messages.content,
-    website: messages.website,
-  })
-  .from(messages)
-  .orderBy(desc(messages.createdAt));
+  let messagesQueryResult = await db
+    .select()
+    .from(messages)
+    .orderBy(desc(messages.createdAt)) as GuestbookMessage[];
+  
+  let repliesQueryResult = await db
+    .select()
+    .from(replies)
+    .orderBy(desc(replies.createdAt)) as GuestbookReply[];
 
-  return savedMessages;
+  let guestbookMessages: GuestbookMessage[] = [];
+  messagesQueryResult.forEach((message) => {
+    message.replies = [];
+    repliesQueryResult.forEach((reply) => {
+      if(reply.messageId === message.id) { message.replies.push(reply); }
+    });
+    message.replies.sort((first, second) =>
+      new Date(first.createdAt).getTime() - new Date(second.createdAt).getTime()
+    );
+    guestbookMessages.push(message);
+  });
+
+  return guestbookMessages;
 };
 
-export const postGuestbookMessage = async (message: Omit<GuestbookMessage, "createdAt">) => {
+export const postGuestbookMessage = async (message: Omit<GuestbookMessage, "id" | "createdAt">) => {
   return await db.insert(messages).values(message).returning({ id: messages.id });
 };
