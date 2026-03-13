bd027f27bee352fd549af19d9e2a81b2e476d1fd
DGCPumarega <gabrielpumarega@yahoo.com>
2026-03-13 15:32:30 -0600

    Edit content

--- src/lib/components/home-sections/Welcome.svelte
+++ src/lib/components/home-sections/Welcome.svelte
@@ -2,7 +2,7 @@
   let {class: className}: {class?: string} = $props();
 </script>
 
-<section class="{className} font-oxanium">
+<section class="{className} font-oxanium max-h-[70vh] scrollable overflow-y-auto">
   <div class="flex gap-x-2 align-middle justify-center mb-4">
     <h2 class="text-xl sm:text-3xl text-center uppercase font-bold">Welcome</h2>
   </div>
@@ -13,18 +13,26 @@
       I studied <span class="font-bold text-red-300">Software Development</span> at the <span class="font-bold text-red-300">Northern Alberta Institute of Technology (NAIT)</span> in Edmonton, and now I'm using what I've learned to make stuff on the internet! 
     </p>
     <p>
-      I was inspired to make my own website after coming across a couple of personal blogs hosted on 
+      I was inspired to start this project after coming across a couple of personal websites hosted on 
       <a class="text-blue-200 underline underline-offset-2" href="https://neocities.org/" target="_blank">Neocities</a> 
       and 
       <a class="text-blue-200 underline underline-offset-2" href="https://nekoweb.org/" target="_blank">Nekoweb</a>. 
-      It was great to see so many sites designed in such creative and personal ways! 
-      They reminded me of how awesome the old internet used to be - back when people used to surf the web instead of visit the same old apps and social media pages.
+      These sites were my introduction to the indie web, and they reminded me that the internet wasn't always about things like 
+      <span class="italic font-light">"maximizing user engagement"</span> or 
+      <span class="italic font-light">"increasing ad revenue"</span>.
+      Back then, websites were a way for people to express themselves creatively and freely. 
     </p>
     <p>
-      This place is <span class="text-yellow-300">still under construction</span>, but I hope you have fun exploring this website!
-      And don't forget to leave a comment (or some constructive criticism) in the 
+      So
+      <span class="text-yellow-300">I made my own personal website 'cause I think it's important to remember the ethos of the old-school of the internet</span> 
+      and keep it alive. 
+      Even if it's just in this small, niche corner of the world wide web.
+    </p>
+    <p>
+      This place is <span class="text-yellow-300">still under construction</span>, so there isn't much to see yet...
+      But I hope you have fun checking out the things I've made so far! And don't forget to leave a comment in the 
       <a class="text-blue-200 underline underline-offset-2" href="#guestbook">guestbook</a> 
-      before you go!
+      before you go! I'm always open to suggestions (or constructive criticism lol)
     </p>
   </div>
 </section>
