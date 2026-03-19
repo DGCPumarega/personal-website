68cf0709286c5d9788b5aec6a8391105b76bd86b
DGCPumarega <gabrielpumarega@yahoo.com>
2026-03-19 04:41:41 -0600

    Edit error page styles

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,4 +1,11 @@
-<section class="max-w-250 mx-auto">
-  <h2>Error 404: Page Not Found</h2>
-  <a href="/">Back to Home</a>
+<section class="max-w-250 mx-auto font-oxanium">
+  <div class="flex flex-col h-[82vh] justify-center">
+    <div class="w-fit self-center">
+      <h2 class="text-xl sm:text-3xl uppercase font-bold">Error 404: Page Not Found</h2>
+      <a class="text-xl self-center" href="/">
+        <span class="font-bold">←</span>
+        <span class="underline underline-offset-2 ">Back to Home</span>
+      </a>
+    </div>
+  </div>
 </section>
