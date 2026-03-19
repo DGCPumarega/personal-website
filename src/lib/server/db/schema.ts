461ac5f9e5d5f2859e62ecb4f75df8649b78e472
DGCPumarega <gabrielpumarega@yahoo.com>
2026-03-19 04:37:22 -0600

    Update schema to use sqlite

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,19 +1,19 @@
-import { pgTable, serial, integer, text, timestamp } from 'drizzle-orm/pg-core';
-import { relations } from 'drizzle-orm';
+import { sqliteTable, integer, text, } from 'drizzle-orm/sqlite-core';
+import { relations, sql } from 'drizzle-orm';
 
-export const messages = pgTable('messages', {
-	id: serial('id').primaryKey(),
+export const messages = sqliteTable('messages', {
+	id: integer('id').primaryKey(),
 	username: text().notNull(),
-	createdAt: timestamp('created_at').notNull().defaultNow(),
+	createdAt: text('created_at').notNull().default(sql`(current_timestamp)`),
 	website: text(),
 	content: text().notNull(),
 });
 
-export const replies = pgTable('replies', {
-	id: serial('id').primaryKey(),
+export const replies = sqliteTable('replies', {
+	id: integer('id').primaryKey(),
 	messageId: integer('message_id').references(() => messages.id).notNull(),
 	username: text().notNull(),
-	createdAt: timestamp('created_at').notNull().defaultNow(),
+	createdAt: text('created_at').notNull().default(sql`(current_timestamp)`),
 	content: text().notNull(),
 });
 
@@ -27,14 +27,3 @@ export const repliesRelations = relations(replies, ({ one }) => ({
 		references: [messages.id]
 	}),
 }))
-
-// Auth Token Store
-export const tokens = pgTable('tokens', {
-	id: serial('id').primaryKey(),
-	createdAt: timestamp('created_at').notNull().defaultNow(),
-	accessToken: text('access_token').notNull(),
-	tokenType: text('token_type').notNull(),
-	expiresIn: integer('expires_in').notNull(),
-	refreshToken: text('refresh_token').notNull(),
-	scope: text('scope').notNull(),
-});
