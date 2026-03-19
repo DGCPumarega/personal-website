ffbba93f3420cb87ff14436003cbf89d15be6627
DGCPumarega <gabrielpumarega@yahoo.com>
2026-03-19 04:42:35 -0600

    Update page server to use D1 db and Workers KV

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,39 +1,41 @@
-import type { 
-  PageServerLoad,
-  Actions
-} from './$types';
-import type {
-  BlogPost,
-  GuestbookMessage,
-  GuestbookReply,
-} from "$lib/types";
+import type { PageServerLoad, Actions } from './$types';
+import type { BlogPost, GuestbookMessage, GuestbookReply, SpotifyTrack } from "$lib/types";
 import { fail } from '@sveltejs/kit';
-import { superValidate } from 'sveltekit-superforms';
+import { message, superValidate } from 'sveltekit-superforms';
 import { zod4 } from 'sveltekit-superforms/adapters';
-import {
-  messageFormSchema,
-  replyFormSchema,
-} from '$lib/components/home-sections/GuestbookSchema';
-import {
-  getGuestbookMessages,
-  postGuestbookMessage,
-  postGuestbookReply,
-} from '$lib/components/home-sections/GuestbookQueries';
-import { loadSpotifyData } from '$lib/components/home-sections/SpotifyDataLoader';
+import { messageFormSchema, replyFormSchema } from '$lib/components/home-sections/GuestbookSchema';
 
 export const load: PageServerLoad = async ({ fetch }) => {
   let blogPostsResponse = await fetch("/api/blog-posts");
   let blogPosts: BlogPost[] = await blogPostsResponse.json();
 
   let guestbookMessages: GuestbookMessage[] | null = null;
-  try {
-    guestbookMessages = await getGuestbookMessages() as GuestbookMessage[];
-  } catch { guestbookMessages = null; }
+
+  let guestbookGetMessagesResponse = await fetch("api/guestbook/get-messages");
+  if(guestbookGetMessagesResponse.status === 200) {
+    let data: { messages: GuestbookMessage[] } = await guestbookGetMessagesResponse.json();
+    guestbookMessages = data.messages;
+  }
 
   let statusCafeResponse = await fetch("https://status.cafe/users/dgcpumarega/status.json")
   let status = await statusCafeResponse.json();
 
-  let { nowPlaying, recentTracks, topTracks } = await loadSpotifyData();
+  let nowPlaying: SpotifyTrack | null = null;
+  let recentTracks: SpotifyTrack[] | null = null;
+  let topTracks: SpotifyTrack[] | null = null;
+
+  let spotifyGetDataResponse = await fetch("/api/spotify/get-data");
+  if(spotifyGetDataResponse.status === 200) {
+    let spotifyData: {
+      nowPlaying: SpotifyTrack | null,
+      recentTracks: SpotifyTrack[] | null,
+      topTracks: SpotifyTrack[] | null
+    } = await spotifyGetDataResponse.json();
+
+    nowPlaying = spotifyData.nowPlaying;
+    recentTracks = spotifyData.recentTracks;
+    topTracks = spotifyData.topTracks;
+  }
 
   return {
     blogPosts: blogPosts,
@@ -55,14 +57,26 @@ export const actions: Actions = {
       return fail(400, { messageForm });
     }
 
-    let message: Omit<GuestbookMessage, "id" | "createdAt"> = {
-      username: messageForm.data.username,
-      content: messageForm.data.content,
-      website: (messageForm.data.website ? messageForm.data.website : null),
-      replies: [],
-    };
+    // let message: Omit<GuestbookMessage, "id" | "createdAt"> = {
+    //   username: messageForm.data.username,
+    //   content: messageForm.data.content,
+    //   website: (messageForm.data.website ? messageForm.data.website : null),
+    //   replies: [],
+    // };
 
-    await postGuestbookMessage(message);
+    const response = await event.fetch("api/guestbook/post-message", {
+      method: "POST",
+      body: JSON.stringify({
+        username: messageForm.data.username,
+        content: messageForm.data.content,
+        website: messageForm.data.website,
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    if(response.status >= 500) { console.error("Unable to Post Message"); }
+
     return { messageForm };
   },
   reply: async (event) => {
@@ -72,13 +86,25 @@ export const actions: Actions = {
       return fail(400, { replyForm });
     }
 
-    let reply: Omit<GuestbookReply, "id" | "createdAt"> = {
-      messageId: replyForm.data.messageId,
-      username: replyForm.data.username,
-      content: replyForm.data.content,
-    };
+    // let reply: Omit<GuestbookReply, "id" | "createdAt"> = {
+    //   messageId: replyForm.data.messageId,
+    //   username: replyForm.data.username,
+    //   content: replyForm.data.content,
+    // };
 
-    await postGuestbookReply(reply);
+    const response = await event.fetch("api/guestbook/post-reply", {
+      method: "POST",
+      body: JSON.stringify({
+        messageId: replyForm.data.messageId,
+        username: replyForm.data.username,
+        content: replyForm.data.content,
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    if(response.status >= 500) { console.error("Unable to Post Reply"); }
+
     return { replyForm };
   },
 };
